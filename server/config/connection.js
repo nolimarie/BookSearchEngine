df2aceb4f72862a013647659f1063131a656223a
agate <nolimarie@protonmail.com>
2022-01-27 23:40:57 -0700

    updated mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksearchDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksearch', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
